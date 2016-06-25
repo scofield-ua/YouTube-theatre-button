39a04ece0b67c6640c0d8256aaf45a6e73fa3202
Dmitriy Scofield <dimka.tkach@gmail.com>
2016-06-25 18:42:23 +0300

    Remove old player support

--- extension/files/youtube-theatre-button/js/content.js
+++ extension/files/youtube-theatre-button/js/content.js
@@ -10,38 +10,34 @@ try {
 
 if (!window.jQuery) execute = false;
 
-if (execute ) {
-    var theatreButton =
-        '<div class="ytp-menuitem" aria-haspopup="true" tabindex="36" role="menuitem" id="yt-theatre-button" data-active="0">'+
-        '<div class="ytp-menuitem-label">Theatre mode</div>'+
-        '<div class="ytp-menuitem-content" style="background-image:none"><div></div></div>'
-        '</div>';
-    var theatreButtonOld = 
-        '<div class="ytp-menu-row">' +
-            '<div class="ytp-menu-cell ytp-menu-title" id="ytp-menu-quality">' +
-                '<div class="ytp-button" role="button" id="yt-theatre-button" style="float:right">Theatre mode</div>' +
-            '</div>'        
-        '</div>';
-    
-    if($('#movie_player #ytp-main-menu-id').length) {
-        if ($('#movie_player #ytp-main-menu-id #yt-theatre-button').length == 0) {
-            $('#movie_player #ytp-main-menu-id').append(theatreButton);    
-            $('#yt-theatre-button').click(goFullWindow);
-        }
-    } else if($('#movie_player .ytp-menu-container').length) {        
-        var exists = $('#movie_player .ytp-menu-container #yt-theatre-button').length;    
-        if(exists == 0) {
-            mainInt = setInterval(function() {            
-                exists = $('#movie_player .ytp-menu-container #yt-theatre-button').length;
+if (execute) {
+    var buttonContainer = document.querySelector('#movie_player .ytp-settings-menu');
 
-                if(exists) {
-                    clearInterval(mainInt);
-                } else {
-                    $('#movie_player .ytp-menu-container .ytp-menu .ytp-menu-content').append(theatreButtonOld);                
-                    $('#yt-theatre-button').click(goFullWindow);
-                }            
-            }, 500);
+    var theatreButton = document.createElement('div');
+    theatreButton.setAttribute('class', 'ytp-menuitem');
+    theatreButton.setAttribute('id', 'yt-theatre-button');
+    theatreButton.setAttribute('role', 'menuitem');
+    theatreButton.innerHtml =
+        '<div class="ytp-menuitem-label">Theatre Mode</div>'+
+        '<div class="ytp-menuitem-content" style="background-image:none"><div></div></div>';
+
+    theatreButton.onclick = goFullWindow;
+
+    /*var theatreButton =
+        '<div class="ytp-menuitem" role="menuitem" id="yt-theatre-button" data-active="0">'+
+            '<div class="ytp-menuitem-label">Theatre Mode</div>'+
+            '<div class="ytp-menuitem-content" style="background-image:none"><div></div></div>'
+        '</div>';*/
+
+    if(buttonContainer.length) {
+        if(buttonContainer.querySelector('#yt-theatre-button').length == 0) {
+            buttonContainer.appendChild(theatreButton);
         }
+
+        /*if($('#movie_player #ytp-main-menu-id #yt-theatre-button').length == 0) {
+            $('#movie_player #ytp-main-menu-id').append(theatreButton);
+            $('#yt-theatre-button').click(goFullWindow);
+        }*/
     }
 }
 
@@ -49,18 +45,18 @@ function goFullWindow() {
     var closeCurrentTab = false;
     chrome.storage.sync.get(['closeCurrentTab'], function(items) {
         closeCurrentTab = items.closeCurrentTab;
-        
+
         var isPaused = $('#player-api .html5-video-player').hasClass('paused-mode') ? true : false;
         if (!isPaused) {
             $('#movie_player .ytp-play-button.ytp-button').click();
-        }    
-        
+        }
+
         var start;
         start = $('#player-api .ytp-time-display .ytp-time-current').text().split(':');
         start = start[0] * 1 > 0 ? ((start[0] * 1) * 60) + (start[1] * 1) : start[1] * 1;
 
         var url = "http://www.youtube.com/embed/"+videoId+"?html5=1&autoplay=1&start="+start;
-        
+
         if (closeCurrentTab) {
             chrome.runtime.sendMessage({method: "changeCurrentTabUrl", url : url});
         } else {
